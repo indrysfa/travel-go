9c48a86afd750d402ec1451115d35d543e8f9d0d
indrysfa <indrysfa@gmail.com>
2021-01-18 22:44:47 +0700

    tambah promotion

--- resources/views/frontend/promotion/index.blade.php
+++ resources/views/frontend/promotion/index.blade.php
@@ -1,5 +1,50 @@
 @extends('frontend.layouts.app')
 @section('title', 'Promotion')
 @section('content')
-    
+    <div class="container">
+        <img src="{{ asset('assets/eforlad/images/headline.jpg') }}" class="img-fluid" alt="gambarh headlinedoor">
+    </div>
+    <br>
+    <div class="row">
+        <div class="col-sm-3">
+            <div class="card" style="width: 18rem;">
+                <img src="{{ asset('assets/eforlad/images/rom.jpg') }}">
+                <div class="card-body" style="text-align: center;">
+                    <h5 class="card-title"><b>899.999 / Hari</b></h5>
+                    <p class="card-text">Discount Up To 50%</p>
+                    <a href="#" class="btn btn-warning">OPEN THE DOOR NOW</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-3">
+            <div class="card" style="width: 18rem;">
+                <img src="{{ asset('assets/eforlad/images/rom2.jpg') }}">
+                <div class="card-body" style="text-align: center;">
+                    <h5 class="card-title"><b>599.999 / Hari</b></h5>
+                    <p class="card-text">Discount Up To 50%</p>
+                    <a href="#" class="btn btn-warning">OPEN THE DOOR NOW</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-3">
+            <div class="card" style="width: 18rem;">
+                <img src="{{ asset('assets/eforlad/images/rom3.jpg') }}">
+                <div class="card-body" style="text-align: center;">
+                    <h5 class="card-title"><b>799.999 / Hari</b></h5>
+                    <p class="card-text">Discount Up To 50%</p>
+                    <a href="#" class="btn btn-warning">OPEN THE DOOR NOW</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-3">
+            <div class="card" style="width: 18rem;">
+                <img src="{{ asset('assets/eforlad/images/rom4.jpg') }}">
+                <div class="card-body" style="text-align: center;">
+                    <h5 class="card-title"><b>999.999 / Hari</b></h5>
+                    <p class="card-text">Discount Up To 50%</p>
+                    <a href="#" class="btn btn-warning">OPEN THE DOOR NOW</a>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
