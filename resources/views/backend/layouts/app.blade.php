d5c5318b1128c12365a9b33d09ac9553f052c4ec
hendhendy <hendy.maxv@gmail.com>
2021-01-22 01:18:25 +0700

    setting

--- resources/views/backend/layouts/app.blade.php
+++ resources/views/backend/layouts/app.blade.php
@@ -125,7 +125,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
                             </a>
                             <ul class="nav nav-treeview">
                                 <li class="nav-item">
-                                    <a href="#" class="nav-link">
+                                    <a href="{{route('backend.sewa.index')}} " class="nav-link">
                                         <i class="far fa-circle nav-icon"></i>
                                         <p>Data</p>
                                     </a>
