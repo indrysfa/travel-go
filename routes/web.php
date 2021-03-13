184e31f2a19ef37090f81ce5d2f212f5d16a6510
hendhendy <hendy.maxv@gmail.com>
2021-03-13 20:03:58 +0700

    UPDATE ROUTE LAGI DEH

--- routes/web.php
+++ routes/web.php
@@ -35,10 +35,6 @@ Route::group(['prefix' => '/', 'namespace' => 'Frontend'], function () {
     //Hendy
     Route::get('/sewa', 'SewaController@indexFrontendSewaDataProperty')->name('frontend.sewa.index');
     Route::get('/cari-property', 'HomeController@frontendSearch')->name('search');
-    Route::get('filterprice', 
-      ['as' => 'price.filter',
-      'uses' => 'HomeController@filterPrice']);
-
 
     // Ika
     Route::get('/promotion', 'PromotionController@promotion')->name('frontend.promotion.index');
